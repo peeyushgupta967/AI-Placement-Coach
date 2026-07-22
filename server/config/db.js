7a29b9effe75c955d6849d6e6436fcd2ec99812d
peeyushgupta967 <peeyushgupta967@gmail.com>
2026-07-23 02:44:12 +0530

    Configure Aiven database for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,30 @@
 const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(
+            path.join(__dirname, "ca.pem")
+        )
+    }
+
+});
+db.connect((err) => {
+
+    if (err) {
+        console.error("Database connection failed:", err.message);
+        return;
+    }
+
+    console.log("Aiven MySQL connected successfully");
+
 });
 
 module.exports = db;
